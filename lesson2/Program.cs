e88a7eed75c162556b97dfcfca810232366f3931
Helen <>
2022-12-04 16:43:52 +0300

    lesson2

---
+++ lesson2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10,99]
+// и показывает наибольшую цифру числа.
+// 78 -> 8; 12->2; 85->8
+
+Random random = new Random();
+
+int randomNumber = random.Next (1,100);
+int lastDigit = randomNumber % 10;
+int first =  randomNumber / 10 % 10;
+
+Console.WriteLine (randomNumber);
+
+if (lastDigit > first)
+{
+   Console.WriteLine (lastDigit);
+}
+else
+{
+
+    Console.WriteLine (first);
+
+}
+
+
+
