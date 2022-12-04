795525b5b81e86b0a81b490b6f44fdf5217a2423
Helen <>
2022-12-04 17:54:50 +0300

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, 
+//является ли первое число кратным второму. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4 16, 4 -> кратно
+
+
+Random random = new Random();
+
+int firstNumber = random.Next (10,100);
+int secondNumber = random.Next (1,10); 
+
+Console. WriteLine (firstNumber);
+Console. WriteLine (secondNumber);
+
+int newDigit = (firstNumber % secondNumber);
+
+if ((firstNumber % secondNumber) !=0)
+
+{
+    Console. WriteLine (newDigit);
+}
+else 
+{
+    Console.WriteLine ("Число кратно");
+}
